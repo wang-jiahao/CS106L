cce24d9c0ac678b2a3059d1f16fa375d5535b095
wang-jiahao <2328796228@qq.com>
2025-01-08 16:18:16 +0800

    update since I have reinstall my system

--- 7-Algorithms/cmake-build-debug/cmake_install.cmake
+++ 7-Algorithms/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Codes/cpp/CS106L/7-Algorithms
+# Install script for directory: D:/Codes/C++/CS106L/7-Algorithms
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "D:/Codes/cpp/CS106L/7-Algorithms/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "D:/Codes/C++/CS106L/7-Algorithms/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
